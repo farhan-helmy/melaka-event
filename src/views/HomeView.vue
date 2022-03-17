86cf238f4f2a8f42f1bef114b85d6870b9a50620
farhan-helmy <farhanhlmy@gmail.com>
2022-03-17 08:08:22 +0800

    alter logo

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,40 @@
 <template>
-  <label class="swap swap-flip text-9xl">
-    <!-- this hidden checkbox controls the state -->
-    <input type="checkbox" />
+  <div class="card w-96">
+    <div class="card-body">
+      <div class="items-center text-center">
+        <label class="swap swap-flip text-9xl">
+          <!-- this hidden checkbox controls the state -->
+          <input type="checkbox" />
 
-    <div class="swap-on">
-      <div class="w-32">
-        <img src="@/assets/logo2.png" alt="" />
+          <div class="swap-on">
+            <div class="w-32">
+              <img src="@/assets/logo2.png" alt="" />
+            </div>
+          </div>
+
+          <div class="swap-off">
+            <div class="w-32">
+              <img src="@/assets/logo_puspanita.png" alt="" />
+            </div>
+          </div>
+        </label>
+        <h3 class="text-5xl font-bold text-indigo-500 animate-pulse">
+          Puspanita
+        </h3>
+
+        <div class="btn" @click="nextPage()">Click to start</div>
+        <footer class="footer p-10 text-neutral-content">
+          <div class="pt-6">
+            <img
+              src="@/assets/logoarkib.png"
+              alt="Shoes"
+              class="rounded-xl object-scale-down h-48 w-96"
+            />
+          </div>
+        </footer>
       </div>
     </div>
-
-    <div class="swap-off">
-      <div class="w-32">
-        <img src="@/assets/logo_puspanita.png" alt="" />
-      </div>
-    </div>
-  </label>
-  <h3 class="text-5xl font-bold text-indigo-500 animate-pulse">Puspanita</h3>
-
-  <div class="btn" @click="nextPage()">Click to start</div>
-
-  <p>By:</p>
-  <img
-    src="@/assets/logoarkib.png"
-    alt="Shoes"
-    class="rounded-xl object-scale-down h-48 w-96"
-  />
+  </div>
 </template>
 
 <script>
